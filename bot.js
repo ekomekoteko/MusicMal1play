c1b4c80ef35c08ff31f73fa0310eac9f4d7c0e86
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-28 06:19:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "g!";
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
@@ -317,7 +317,20 @@ function play(guild, song) {
 }
 });
 
+client.on('message', msg => {
 
+    if (msg.content == '1join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("526430911131156490").join(); 
+    });
 
 
 
